de58b93a0f15aec35f99149885ba85d824c7837a
mrb4l4ge <kolbeb@gmail.com>
2025-03-28 22:43:18 +0100

    class imp. Board, Cell

--- src/solver.py
+++ src/solver.py
@@ -1,19 +1,36 @@
-
-
-class Solver:
-    '''Calculates board states'''
+class Board:
+    '''Represent the board made of 9x9 cells'''
 
     def __init__(self):
+        self.board = []
+        self.setup_board()
+    
+    def setup_board(self):
+        '''Setup the board frpom cell objects'''
 
-        '''Initial board setup. Fill board with numbers'''
-        
-        self.current_board_state = [[[num+1 for num in range(9)] for row in range(9)] for col in range(9)]
-        
+        for row in range(9):
+            for col in range(9):
+                self.board.append(Cell(row,col))
+
+
+class Cell:
+    '''Represent one cell in the board'''
+
+    def __init__(self, pos_r, pos_c):
+        self.pos_r = pos_r
+        self.pos_c = pos_c
+        self.num_list = [n+1 for n in range(9)]
+
+
+class BoardStateUpdater:
+    '''Calculates board states'''
+
+    def __init__(self, board):
+        self.board = board
         self.block_valuemap = {1: [0,1,2],
                                2: [3,4,5],
                                3: [6,7,8],
                                }
-
 
     def elim_num_from_row(self, row, num):
         '''Eliminate invalid number in all cells of a row'''
@@ -51,15 +68,15 @@ class Solver:
         self.elim_num_from_col(col, num)
         self.elim_num_from_block(row, col, num)
 
-    def calc_row(self, row):
+    def check_single_num_in_row(self, row):
         '''Checks if only one cell contain a number in a row'''
         pass
 
-    def calc_column(self, column):
+    def check_single_num_in_col(self, column):
         '''Checks if only one cell contain a number in a column'''
         pass
 
-    def calc_block(self, block):
+    def check_single_num_in_block(self, block):
         '''Checks if only one cell contain a number in a block'''
         pass
 
